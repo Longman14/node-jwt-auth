3ccb84cb06360ea1492ee3695531053ab8a6a015
Longman14 <hassandamilola580@gmail.com>
2023-10-04 09:23:18 +0100

    added authentication middleware

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,2 +1,22 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+function authMiddleware (req, res, next){
+    //Get the token from the request header
+    
+    const token = req.header('Authorization');
+
+    //check if token is missing
+    if (!token){
+        return res.status(401).json({error: 'Authentication failed: No token provided'});
+
+    }
+    try {
+        //verify the token using jwt secret
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET );
+        req.user = decoded;
+        //continue to the next route
+        next();
+    }catch (err){
+        res.status(401).json({error: 'Authentication failed: Invalid token '})
+    }
+}
